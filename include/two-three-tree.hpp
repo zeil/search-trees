b13e5b9fec3d3ad6b172ce2831873769a0e8f46e
Dmitry Yakunin <d.yakunin@rambler-co.ru>
2017-12-14 10:38:31 +0300

    make_unique for old compilers

--- include/two-three-tree.hpp
+++ include/two-three-tree.hpp
@@ -6,6 +6,18 @@
 
 #include "search-tree.hpp"
 
+#if __cplusplus < 201402L
+namespace std
+{
+// https://herbsutter.com/gotw/_102/
+template<typename T, typename ...Args>
+std::unique_ptr<T> make_unique( Args&& ...args )
+{
+	return std::unique_ptr<T>( new T( std::forward<Args>(args)... ) );
+}
+} // namespace std
+#endif // __cplusplus < 201402L
+
 template<typename Key, typename Value>
 class TwoThreeTree final: public SearchTree<Key, Value>
 {
