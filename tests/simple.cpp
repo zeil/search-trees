47a432c386314efede5559b49eafcb465807db63
Dmitry Yakunin <d.yakunin@rambler-co.ru>
2017-12-14 13:17:14 +0300

    Use of abstract class in test

--- tests/simple.cpp
+++ tests/simple.cpp
@@ -8,22 +8,23 @@
 
 int main()
 {
-	TwoThreeTree<char, int> tree1, tree2;
-	tree1.insert('A', 0);
-	tree1.insert('L', 0);
-	tree1.insert('G', 0);
-	tree1.insert('O', 0);
-	tree1.insert('R', 0);
-	tree1.insert('I', 0);
-	tree1.insert('T', 0);
-	tree1.insert('H', 0);
-	tree1.insert('M', 0);
-	tree1.insert('S', 0);
-	tree1.print(std::cout);
+	std::unique_ptr<SearchTree<char, int>> tree1 = std::make_unique<TwoThreeTree<char, int>>();
+	tree1->insert('A', 0);
+	tree1->insert('L', 0);
+	tree1->insert('G', 0);
+	tree1->insert('O', 0);
+	tree1->insert('R', 0);
+	tree1->insert('I', 0);
+	tree1->insert('T', 0);
+	tree1->insert('H', 0);
+	tree1->insert('M', 0);
+	tree1->insert('S', 0);
+	tree1->print(std::cout);
 
+	std::unique_ptr<SearchTree<char, int>> tree2 = std::make_unique<TwoThreeTree<char, int>>();
 	for (char c = 'A'; c <= 'Z'; ++c)
-		tree2.insert(c, 0);
-	tree2.print(std::cout);
+		tree2->insert(c, 0);
+	tree2->print(std::cout);
 
 #ifdef _WIN32
 	_CrtDumpMemoryLeaks();
